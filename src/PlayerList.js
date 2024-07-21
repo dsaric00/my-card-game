e3158fecffefb2778e1802e55da3b1ece8f213b4
David Šarić <151877531+dsaric00@users.noreply.github.com>
2024-07-21 22:47:12 +0200

    Update PlayerList.js

--- src/PlayerList.js
+++ src/PlayerList.js
@@ -1,42 +1,32 @@
 import React, { useState, useEffect } from 'react';
-import { fetchPlayers } from './playerService'; // Uvezi funkciju fetchPlayers
 import 'bootstrap/dist/css/bootstrap.min.css';
 
-
-
-//Komponenta 
-const PlayerList = ({ onSelect}) => {
+const PlayerList = ({ players, onSelect }) => {
   // state varijable 'players' i funkcije 'setPlayers' prazan niz za ažuriranje stanja 
-  const [players, setPlayers] = useState([]);
-// useEffect hook za dohvaćanje podataka kada se komponenta montira 
-  useEffect(() => {
-    // Asinkrona funkcija za dovaćanje podataka 
-    const getPlayers = async () => {
-      try {
-        const playersData = await fetchPlayers(); // Pozovi fetchPlayers funkciju
-        setPlayers(playersData); // Postavi dobivene igrače u stanje komponente
-      } catch (error) {
-        console.error('Error fetching players:', error); //ispis greške u konzolu u slučaju errora 
-      }
-    };
+  const [localPlayers, setLocalPlayers] = useState(players);
 
-    getPlayers(); // Pozovi funkciju za dohvat igrača kada se komponenta montira
-  }, []);// Prazan niz kao drugi argument, useEffect će se pokrenuti samo jednom nakon što se komponenta montira 
-  //Renderiranje JSX-a
+  useEffect(() => {
+    setLocalPlayers(players);
+  }, [players]);
+
   return (
     <div>
-    <h2>Overview</h2>
-    <div className="card-flex">
-      {players.map((player) => {
-        return (
-          <div className="card" key={player.id} onClick={() => onSelect(player)} >
-            {player.playerName}
-          </div>
-        );
-      })}
+      <h2>Overview</h2>
+      <div className="card-flex">
+        {localPlayers.map((player) => {
+          return (
+            <div className="card" key={player.id} onClick={() => onSelect(player)}>
+              <p>{player.realName}</p>
+              <p>{player.playerName}</p>
+              <p>{player.asset}</p>
+            </div>
+          );
+        })}
+      </div>
     </div>
-  </div>
   );
 };
+
 // izvoz komponente kako bi se mogla koristit
 export default PlayerList;
+
